0e827c97babe4fb7df85cae6cb763798677a4a70
Peegvig <arumallavihal@gmail.com>
2023-12-03 16:56:18 -0600

    cleaned up help msg.

--- src/main/java/com/CHChestFinder/SimpleCommands.java
+++ src/main/java/com/CHChestFinder/SimpleCommands.java
@@ -16,23 +16,24 @@ public class SimpleCommands extends CommandBase {
 
     @Override
     public String getCommandUsage(ICommandSender sender) {
-        return "chscf <on/off/help>";
+        return "chscf <on/off/>";
     }
 
     @Override
     public void processCommand(ICommandSender sender, String[] args) {
 
         if(args[0].equals("help")) {
-            Minecraft.getMinecraft().thePlayer.addChatMessage(new ChatComponentText("This is the Crystal Hollows Structure Chest Finder (chscf)\\nCommands:\\n/chscf on - enables the mod\\n/chscf off - disables the mod\\nchscf - shows the command menu").setChatStyle(new ChatStyle().setColor(EnumChatFormatting.BLUE)));
+            Minecraft.getMinecraft().thePlayer.addChatMessage(new ChatComponentText("This is the Crystal Hollows Structure Chest Finder (chscf). Commands: /chscf aw on - enables the mod. /chscf off - disables the mod.").setChatStyle(new ChatStyle().setColor(EnumChatFormatting.BLUE)));
         }
         else if(args[0].equals("auto_waypoint") || args[0].equals("aw")){
             if(args[1].equals("on")){
-            	 if (args[2].matches("[0-9]")) {
-                 	CHChestFinder.seconds=Integer.parseInt(args[2]);
-                 }
-                 else {
-                 	CHChestFinder.seconds = 10;
-                 }
+            	CHChestFinder.seconds = 2;
+//            	 if (args[2].matches("[0-9]")) {
+//                 	CHChestFinder.seconds=Integer.parseInt(args[2]);
+//                 }
+//                 else {
+//                 	CHChestFinder.seconds = 10;
+//                 }
                 CHChestFinder.autoWaypoint=true;
                 Minecraft.getMinecraft().thePlayer.addChatMessage(new ChatComponentText("Auto Waypoint enabled!").setChatStyle(new ChatStyle().setColor(EnumChatFormatting.BLUE)));
             }
